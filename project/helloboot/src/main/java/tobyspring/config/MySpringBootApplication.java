61c68739de3b8d68406551b473ab42ef02ddb5b2
eoaud <eoaudehd0818@naver.com>
2023-02-19 15:25:19 +0900

    study spring boot first commit

---
+++ project/helloboot/src/main/java/tobyspring/config/MySpringBootApplication.java
@@ -0,0 +1,20 @@
+package tobyspring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.config.autoconfig.TomcatWebServletConfig;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration
+@ComponentScan
+@EnableMyAutoConfiguration
+public @interface MySpringBootApplication {
+}
