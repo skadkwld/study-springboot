51caff679c767ae2f0bef7a496621dd7d66f8a7f
eoaud <eoaudehd0818@naver.com>
2023-03-02 22:04:43 +0900

    properties 를 클래스에 매핑해서 Bean으로 만든뒤 자동주입되게끔 설정

--- project/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServletConfig.java
+++ project/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServletConfig.java
@@ -1,27 +1,27 @@
 package tobyspring.config.autoconfig;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
-import org.springframework.core.type.AnnotatedTypeMetadata;
-import org.springframework.util.ClassUtils;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@Import(ServerProperties.class)
 public class TomcatWebServletConfig {
-    @Value("${contextPath}")
-    String contextPath;
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env){
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties properties){
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(contextPath);
+
+        factory.setContextPath(properties.getContextPath());
+        factory.setPort(properties.getPort());
+
         return factory;
     }
+
 }
